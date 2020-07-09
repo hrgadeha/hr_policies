d4a5794d55e0965a278b29dcb65d05ca8dc0e744
root <root@ip-172-26-11-196.ap-south-1.compute.internal>
2020-07-09 15:30:14 +0000

    Module Added

---
+++ hr_policies/config/hr_policies.py
@@ -0,0 +1,39 @@
+from __future__ import unicode_literals
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"label": _("Policies"),
+			"items": [
+				{
+                                        "type": "doctype",
+                                        "name": "Loan Policies",
+                                        "label": "Loan Policies",
+                                        "description": _("Loan Policies"),
+                                        "onboard": 1
+                                },
+				{
+                                        "type": "doctype",
+                                        "name": "Attendance Policies",
+                                        "label": "Attendance Policies",
+                                        "description": _("Attendance Policies"),
+                                        "onboard": 1
+                                },
+				{
+					"type": "doctype",
+					"name": "Referral Bonus Policies",
+					"label": "Referral Bonus Policies",
+					"description": _("Referral Bonus Policies"),
+					"onboard": 1
+				},
+				{
+					"type": "doctype",
+					"name": "Advance Salary Policies",
+					"label": "Advance Salary Policies",
+					"description": _("Advance Salary Policies"),
+					"onboard": 1
+				}
+			]
+		}
+]
