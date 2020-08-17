c0a9581e0bd9852c8c1de16b847613b6213b5adf
root <root@ip-172-26-11-196.ap-south-1.compute.internal>
2020-08-17 08:03:47 +0000

    Removed

--- hr_policies/hr_policies/doctype/salary_advance/salary_advance.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2020, Hardik gadesha and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Salary Advance', {
-	// refresh: function(frm) {
-
-	// }
-});
