2b0902048a64a39f5ed22e677b448d57b0aad070
root <root@ip-172-26-11-196.ap-south-1.compute.internal>
2020-09-23 18:58:48 +0000

    Miss Punch JS

--- hr_policies/hr_policies/doctype/miss_punch_application/miss_punch_application.js
+++ hr_policies/hr_policies/doctype/miss_punch_application/miss_punch_application.js
@@ -6,3 +6,62 @@ frappe.ui.form.on('Miss Punch Application', {
 
 	// }
 });
+
+frappe.ui.form.on("Miss Punch Application", {
+  "miss_punch_date": function(frm) {
+	if(frm.doc.employee){
+    frappe.call({
+    "method": "hr_policies.hr_policies.doctype.miss_punch_application.miss_punch_application.getattendance",
+args: {
+employee: frm.doc.employee,
+attendance_date: frm.doc.miss_punch_date
+},
+callback:function(r){
+	var len=r.message.length;
+	    if(!r.message){
+	        frm.set_value("last_punch_time","");
+	        frm.set_value("attendance","");
+	        frappe.throw("No Miss Punch Found On Selected Date, Please Select Valid Date");
+	    }
+	    else{
+	        frm.set_value("last_punch_time",r.message[0][1]);
+	        frm.set_value("attendance",r.message[0][0]);
+	    }
+	}
+    });
+
+	}
+	}
+});
+
+
+frappe.ui.form.on("Miss Punch Application", {
+  "miss_punch_date": function(frm) {
+	if(frm.doc.employee){
+    frappe.call({
+    "method": "hr_policies.hr_policies.doctype.miss_punch_application.miss_punch_application.getlapp",
+args: {
+employee: frm.doc.employee,
+attendance_date: frm.doc.miss_punch_date
+},
+callback:function(r){
+	var len=r.message.length;
+	    if(!r.message){
+	        frm.set_value("leave_application","");
+	        frappe.throw("No Miss Punch Found On Selected Date, Please Select Valid Date");
+	    }
+	    else{
+	        frm.set_value("leave_application",r.message[0][0]);
+	    }
+	}
+    });
+}
+}
+});
+
+frappe.ui.form.on('Miss Punch Application',  'validate',  function(frm) {
+    if (!frm.doc.attendance || !frm.doc.leave_application) {
+        frappe.throw("No Miss Punch Found On Selected Date, Please Select Valid Date");
+        validated = false;
+    }
+});
