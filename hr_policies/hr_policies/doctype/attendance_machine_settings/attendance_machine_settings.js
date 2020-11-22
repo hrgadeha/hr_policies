3787eb6c42be7e8d972050393d1e545cb591a444
root <root@ip-172-26-11-196.ap-south-1.compute.internal>
2020-11-22 16:57:55 +0000

    JS Updated

--- hr_policies/hr_policies/doctype/attendance_machine_settings/attendance_machine_settings.js
+++ hr_policies/hr_policies/doctype/attendance_machine_settings/attendance_machine_settings.js
@@ -6,3 +6,21 @@ frappe.ui.form.on('Attendance Machine Settings', {
 
 	// }
 });
+
+
+frappe.ui.form.on('Attendance Machine Settings', {
+	process_attendance_manually(frm) {
+    frappe.call({
+        "method": "hr_policies.attendance_integration.run_attendance_manually",
+        args: {
+            from_date: frm.doc.from_date,
+            to_date: frm.doc.to_date
+        },
+        callback:function(r){
+            msgprint("Attendance Generated For Date : "+frm.doc.from_date+" To " + frm.doc.to_date);
+    }
+});
+}
+});
+
+
